dbd03405d049318d3586e0d9651a1c65b68adcff
Tianyun Shan <nuynait@gmail.com>
2019-04-11 22:55:36 -0400

    init BLERequest project

---
+++ BLERequest/BLERequest/BLERequest.h
@@ -0,0 +1,19 @@
+//
+//  BLERequest.h
+//  BLERequest
+//
+//  Created by Tianyun Shan on 2019-04-11.
+//  Copyright © 2019 Hourglass Lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BLERequest.
+FOUNDATION_EXPORT double BLERequestVersionNumber;
+
+//! Project version string for BLERequest.
+FOUNDATION_EXPORT const unsigned char BLERequestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BLERequest/PublicHeader.h>
+
+
